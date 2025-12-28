b027d71d19ed4dd899fa9298518f98cc54acab8b
xransum <xransum@users.noreply.github.com>
2025-12-27 21:34:13 -0500

    Add constants for HTTP methods and URI patterns in abuse_reporter

---
+++ src/abuse_reporter/constants.py
@@ -0,0 +1,26 @@
+METHOD_FLAGS = ["POST", "PUT", "DELETE"]
+WHITELISTED_URIS = [
+    r"/static/",
+    r"^.+?favicon.ico",
+    r"^.+?robots.txt",
+    r"^.+?sitemap(\.[a-z]+)?",
+    r"^\/$",
+    r"^\/pp.html$",
+    r"^\/tou.html$",
+]
+URI_FLAGS = [
+    r"(sign-?in)|login|logout|register|create-?account|create-?user|signup|sign-?up",
+    r"(.+)?\.(git|env)(.+)?",
+    r"(.+)?accesson(.+)?",
+    r"(.+)?admin(.+)?",
+    r"(.+)?admin(.+)?",
+    r"(.+)?config(.+)?",
+    r"(.+)?login(.+)?",
+    r"(.+)?uploads(.+)?",
+    r"(.+)?users(.+)?",
+    r"(.+)?wp-?admin(.+)?",
+    r"(.+)?xmlrpc(.+)?",
+    r"(.+)?\/txets\.php",
+    r"(\.alfa)|alfa.+\.php",
+    r"\/node_modules\/",
+]
